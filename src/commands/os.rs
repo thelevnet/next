e2eeebc148f2b685e81d2fe5a1426ac0c09a3e35
thelevnet <dev@lev-net.xyz>
2026-09-13 15:50:37 +0200

    fix(os): pass /etc/nixos to nh os switch and build to silence installable warning

--- src/commands/os.rs
+++ src/commands/os.rs
@@ -3,11 +3,11 @@ use crate::shell::run;
 pub const CLEAN_CMD: &str = "sudo nix-env --delete-generations +2 --profile /nix/var/nix/profiles/system && sudo nix-collect-garbage -d && sudo nix-store --optimise && sudo nix-collect-garbage --delete-old && sudo journalctl --vacuum-time=3d && sudo rm -rf /tmp/* && nix-env --delete-generations +2 && home-manager expire-generations '-1 days' && sudo nix-store --gc";
 
 pub fn switch() -> bool {
-    run("nh os switch")
+    run("nh os switch /etc/nixos")
 }
 
 pub fn dry() -> bool {
-    run("nh os build")
+    run("nh os build /etc/nixos")
 }
 
 pub fn clean() -> bool {
